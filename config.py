907f07aa541bf874d2ee27e60691e8d49a301c43
dorjoo <dorjoohover@gmail.com>
2025-07-24 15:49:46 +0800

    a

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DATA_PATH = os.getenv("DATA_PATH", "assets/Vehicles_ML_last_v_2_5.xlsx")
 PREDICT_PATH = os.getenv("PREDICT_PATH", "assets/predicted_prices_all_data.xlsx")
 OUTPUT_PATH = os.getenv("OUTPUT_PATH", "assets/output_with_app_columns.json")
 HOST       = os.getenv("HOST", "0.0.0.0")
-PORT       = int(os.getenv("PORT", 5000))
+PORT       = int(os.getenv("PORT", 80))
 
 # simple API-key for requests
 API_KEY    = os.getenv("API_KEY", "changeme123")
