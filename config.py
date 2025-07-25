ea8677b11110880142c1115b76eabeedd2a56101
dorjoo <dorjoohover@gmail.com>
2025-07-25 11:26:19 +0800

    'a'

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MODEL_PATH = os.getenv("MODEL_PATH", "model/car_model.pkl")
+MODEL_PATH = os.getenv("MODEL_PATH", "models/car_model.pkl")
 MEDIAN_PATH = os.getenv("MEDIAN_PATH", "assets/median_data.xlsx")
 DATA_PATH = os.getenv("DATA_PATH", "assets/Vehicles_ML_last_v_2_5.xlsx")
 PREDICT_PATH = os.getenv("PREDICT_PATH", "assets/predicted_prices_all_data.xlsx")
