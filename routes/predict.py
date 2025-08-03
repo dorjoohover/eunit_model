7a612f15fb3ad32a1c40b3151263eb2d7dd4822c
dorjoo <dorjoohover@gmail.com>
2025-08-03 13:41:34 +0800

    a

--- routes/predict.py
+++ routes/predict.py
@@ -11,96 +11,96 @@ predict_bp = Blueprint("predict", __name__)
 # Example GET route with hardcoded features
 
 
-# @predict_bp.route("/predict", methods=["GET"])
-# # @require_api_key()
-# def predict_get():
-#     features = {
-#         'brand': 'toyota',
-#         'mark': 'prius-41',
-#         'Engine_capacity': 1.8,
-#         'Year_of_manufacture': 2012,
-#         'Year_of_entry': 2023,
-#         'Gearbox': 'Автомат',
-#         'Hurd': 'Буруу',
-#         'Type': 'Гэр бүлийн',
-#         'Color': 'Саарал',
-#         'Engine': 'Бензин',
-#         'Interior_color': 'Хар',
-#         'Drive': 'Урдаа FWD',
-#         'Mileage': 180000,
-#         'Conditions': 'Дугаар авсан'
-#     }
-#     try:
-#         result = predict(features)
-#         return jsonify({"prediction": result}), 200
-#     except Exception as e:
-#         abort(500, description=str(e))
+@predict_bp.route("/predict", methods=["GET"])
+# @require_api_key()
+def predict_get():
+    features = {
+        'brand': 'toyota',
+        'mark': 'prius-41',
+        'Engine_capacity': 1.8,
+        'Year_of_manufacture': 2012,
+        'Year_of_entry': 2023,
+        'Gearbox': 'Автомат',
+        'Hurd': 'Буруу',
+        'Type': 'Гэр бүлийн',
+        'Color': 'Саарал',
+        'Engine': 'Бензин',
+        'Interior_color': 'Хар',
+        'Drive': 'Урдаа FWD',
+        'Mileage': 180000,
+        'Conditions': 'Дугаар авсан'
+    }
+    try:
+        result = predict(features)
+        return jsonify({"prediction": result}), 200
+    except Exception as e:
+        abort(500, description=str(e))
 
-# # POST route that takes input from client
-
-
-# @predict_bp.route("/predict/car", methods=["POST"])
-# # @require_api_key()
-# def predict_post():
-#     try:
-#         body = request.get_json()
-#         if not body:
-#             abort(400, description="Invalid or missing JSON body")
-#         print(body.get('num'))
-#         vehicle = getVehicle(body.get('num'))
-#         milleage, drive, gearbox = body.get(
-#             'milleage'), body.get('drive'), body.get('gearbox')
-#         print(vehicle)
-#         vin = vehicle.get('cabinNumber')
-#         brand, mark,  buildYear = vehicle.get('markName'), vehicle.get(
-#             'modelName'),  vehicle.get('buildYear')
-#         importedDate = datetime.strptime(
-#             str(vehicle.get('importDate')), '%Y-%m-%d %H:%M:%S%z').year
-#         khurd = 'Буруу' if vehicle.get('wheelPosition') == 'Баруун' else 'Зөв'
-#         color = vehicle.get('colorName')
-#         capacity = round(float(str(vehicle.get('capacity'))) / 1000, 1)
-#         engine = fuel_values(vehicle.get('fueltype'))
-#         normalized = normalize_mark(brand, mark, vin)
-#         brand = normalize_brand(brand)
-#         features = {
-#             'brand': brand,
-#             'mark': normalized,
-#             'Engine_capacity': capacity,
-#             'Year_of_manufacture': buildYear,
-#             'Year_of_entry': importedDate,
-#             'Gearbox': gearbox,
-#             'Hurd': khurd,
-#             'Type': None,
-#             'Color': color,
-#             'Engine': engine,
-#             'Interior_color': None,
-#             'Drive': drive,
-#             'Mileage': milleage,
-#             'Conditions': None
-#         }
-#         print(features)
-#         # return features
-#         result = predict(features)
-#         return jsonify({"prediction": result, "vehicle": vehicle, "features": features}), 200
-
-#     except Exception as e:
-#         print(e)
-#         abort(500, description=str(e))
+# POST route that takes input from client
 
 
-# @predict_bp.route("/vehicle", methods=["POST"])
-# def vehicle():
-#     try:
-#         body = request.get_json()
-#         if not body:
-#             abort(400, description="Invalid or missing JSON body")
-#         vehicle = getVehicle(body.get('num'))
+@predict_bp.route("/predict/car", methods=["POST"])
+# @require_api_key()
+def predict_post():
+    try:
+        body = request.get_json()
+        if not body:
+            abort(400, description="Invalid or missing JSON body")
+        print(body.get('num'))
+        vehicle = getVehicle(body.get('num'))
+        milleage, drive, gearbox = body.get(
+            'milleage'), body.get('drive'), body.get('gearbox')
+        print(vehicle)
+        vin = vehicle.get('cabinNumber')
+        brand, mark,  buildYear = vehicle.get('markName'), vehicle.get(
+            'modelName'),  vehicle.get('buildYear')
+        importedDate = datetime.strptime(
+            str(vehicle.get('importDate')), '%Y-%m-%d %H:%M:%S%z').year
+        khurd = 'Буруу' if vehicle.get('wheelPosition') == 'Баруун' else 'Зөв'
+        color = vehicle.get('colorName')
+        capacity = round(float(str(vehicle.get('capacity'))) / 1000, 1)
+        engine = fuel_values(vehicle.get('fueltype'))
+        normalized = normalize_mark(brand, mark, vin)
+        brand = normalize_brand(brand)
+        features = {
+            'brand': brand,
+            'mark': normalized,
+            'Engine_capacity': capacity,
+            'Year_of_manufacture': buildYear,
+            'Year_of_entry': importedDate,
+            'Gearbox': gearbox,
+            'Hurd': khurd,
+            'Type': None,
+            'Color': color,
+            'Engine': engine,
+            'Interior_color': None,
+            'Drive': drive,
+            'Mileage': milleage,
+            'Conditions': None
+        }
+        print(features)
+        # return features
+        result = predict(features)
+        return jsonify({"prediction": result, "vehicle": vehicle, "features": features}), 200
 
-#         return jsonify({"vehicle": vehicle}), 200
+    except Exception as e:
+        print(e)
+        abort(500, description=str(e))
 
-#     except Exception as e:
-#         print(e)
-#         abort(500, description=str(e))
+
+@predict_bp.route("/vehicle", methods=["POST"])
+def vehicle():
+    try:
+        body = request.get_json()
+        if not body:
+            abort(400, description="Invalid or missing JSON body")
+        vehicle = getVehicle(body.get('num'))
+
+        return jsonify({"vehicle": vehicle}), 200
+
+    except Exception as e:
+        print(e)
+        abort(500, description=str(e))
         
         
 @predict_bp.route("/property", methods=["POST"])
@@ -189,7 +189,7 @@ def getPropertyInfo(arg: str = ''):
         print(citizen)
         res = citizen.dump('WS100201_getPropertyInfo', params).response
         res_dict = serialize_object(res)
-        print(res_dict)
+        print(res_dict.__dict__)
         return res_dict
 
     except Exception as e:
